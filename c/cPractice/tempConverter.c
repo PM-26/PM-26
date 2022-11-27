eb0c4c1422640db2d6ee3ddf5e6e780112e402a0
PM <107318440+PM-26@users.noreply.github.com>
2022-11-27 13:26:56 +0530

    done

--- c/cPractice/tempConverter.c
+++ c/cPractice/tempConverter.c
@@ -3,5 +3,5 @@ int main(){
     float c;
     printf("Enter temperature in Celsius to convert it into Fahrenheit \n");
     scanf("%f",&c);
-    printf("Temperature in Fahrenheit=%f. \n",(c*9/5)+32);
+    printf("Temperature in Fahrenheit=%f= \n",(c*9/5)+32);
 }
